f965a69090d132edeb4b9439c7322c0b8e67fae7
Mugisha-isaac <mugishaisaac2001@gmail.com>
2022-05-24 16:20:56 +0200

    wpi([id].js) getting one post

---
+++ pages/[id].js
@@ -0,0 +1,23 @@
+import supabase from '../utils/supabase'
+
+export async function getServerSideProps({params}){
+  
+    const {data:post,error} = await supabase.from('posts').select('*').eq('id',params.id).single();
+
+    if(error) throw new Error(err.message);
+
+   return{
+       props:{
+           post
+       }
+   }
+}
+
+export default function PostPage({post}){
+    return (
+        <div>
+           <h1>{post.title}</h1>
+           <p>{post.content}</p>
+        </div>
+    )
+}
